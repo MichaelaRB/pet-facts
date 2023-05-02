726a4d4d1cfb57c832eaac1ad6a0ecda9a2a79c9
Michaela Brown <michaelarbrown4@gmail.com>
2023-05-02 08:54:04 -0500

    added code to store a saved fact to local storage and to display the saved facts

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,14 @@ var catFact = "";
 var dogFact = "";
 var imgUrl = "";
 var catButton = document.getElementById("catBtn");
-console.log(catButton);
+var factList = document.getElementById("savedFacts"); 
+var saveButton = document.getElementById("saveFact");
+
 catButton.addEventListener("click", function(){
 //fetching the cat fact and image
-
-fetch(catFactUrl)
+    factList.innerHTML = ""; 
+    saveButton.setAttribute("style","display:inline");
+    fetch(catFactUrl)
     .then(function (response) {
         return response.json();
     })
@@ -34,25 +37,60 @@ fetch(catFactUrl)
 //fetching the dog fact and image
 var dogButton = document.getElementById("dogBtn");
 dogButton.addEventListener("click", function() {
-fetch(dogImgUrl)
+    saveButton.setAttribute("style","display:inline");
+    factList.innerHTML = "";
+    fetch(dogImgUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         imgUrl = data.url;
-        fetch(dogFactUrl)
-            .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            dogFact = data.data[0].attributes.body;
-            var imgEl = document.getElementById("petImg");
-            var dogEl = document.getElementById("petFact");
+    fetch(dogFactUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        dogFact = data.data[0].attributes.body;
+        var imgEl = document.getElementById("petImg");
+        var dogEl = document.getElementById("petFact");
             
-            imgEl.setAttribute("src",imgUrl);
-            imgEl.setAttribute("style", "display: in-line");
-            dogEl.textContent = dogFact;
-        });
+        imgEl.setAttribute("src",imgUrl);
+        imgEl.setAttribute("style", "display: in-line");
+        dogEl.textContent = dogFact;
     });
 });
+});
 
+
+saveButton.addEventListener("click", function(){
+    var factEl = document.getElementById("petFact");
+    var fact = factEl.textContent;
+    var factArray = [];
+    if(localStorage.getItem("facts") !== null) factArray = JSON.parse(localStorage.getItem("facts"));
+    
+    factArray.unshift(fact);
+    localStorage.setItem("facts",JSON.stringify(factArray));
+});
+
+
+var displayButton = document.getElementById("displayFact");
+var savedFacts = [];
+
+
+displayButton.addEventListener("click", function(){
+    var imgEl = document.getElementById("petImg");
+    factList.innerHTML = "";
+    imgEl.setAttribute("src","./assets/images/fact-display-img.jpg");
+
+    var factEl = document.getElementById("petFact");
+    factEl.textContent = "";
+
+    savedFacts = JSON.parse(localStorage.getItem("facts"));
+
+    for(var i = 0; i<savedFacts.length; i++) {
+        var fact = document.createElement("li");
+        fact.textContent = savedFacts[i];
+        factList.appendChild(fact);
+        fact.setAttribute("style", "font-size: 15px; margin-bottom: 7px; padding-left: 5px;");
+    }
+});
